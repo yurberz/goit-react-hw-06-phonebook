40b10c101d83c9e164cdfe246eba3416445057bb
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-01-29 20:13:37 +0200

    fix addContact reducer

--- src/redux/contacts/contactsReducer.js
+++ src/redux/contacts/contactsReducer.js
@@ -3,27 +3,7 @@ import { createReducer } from "@reduxjs/toolkit";
 import contactsActions from "./contactsActions";
 
 const addContact = (state, action) => {
-  if (
-    state &&
-    state.find(
-      (contact) =>
-        action.payload.contact.name.toLowerCase() === contact.name.toLowerCase()
-    )
-  ) {
-    alert("Is already in use!");
-  } else if (
-    state.find((contact) => action.payload.contact.number === contact.number)
-  ) {
-    alert("Is already in use!");
-  } else if (
-    action.payload.contact.name.trim() === "" ||
-    action.payload.contact.number.trim() === ""
-  ) {
-    alert("Enter the contact's name and(or) phone number!");
-  } else {
-    return [...state, action.payload.contact];
-  }
-  // return [...state, action.payload.contact];
+  return [...state, action.payload.contact];
 };
 
 const delContact = (state, action) => {
